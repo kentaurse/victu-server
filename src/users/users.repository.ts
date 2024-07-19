fae09dbc99c10cdcbc98624ae6f8173648eb5282
johnsmith0212 <john0212.skill@gmail.com>
2024-07-19 18:19:31 +0900

    refactoring, auth folder added

---
+++ src/users/users.repository.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { FilterQuery, Model } from 'mongoose';
+import { User, UserDocument } from './schemas/user.schema';
+
+@Injectable()
+export class UsersRepository {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async findOne(userFilterQuery: FilterQuery<User>): Promise<User> {
+    return this.userModel.findOne(userFilterQuery);
+  }
+
+  async find(userFilterQuery: FilterQuery<User>): Promise<User[]> {
+    return this.userModel.find(userFilterQuery);
+  }
+
+  async create(user: User): Promise<User> {
+    return this.userModel.create(user);
+  }
+
+  async findOneAndUpdate(
+    userFilterQuery: FilterQuery<User>,
+    user: Partial<User>,
+  ): Promise<User> {
+    return this.userModel.findOneAndUpdate(userFilterQuery, user);
+  }
+}
