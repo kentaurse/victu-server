2180b40a0a04e016655314067eaff94e031f313a
glory1001 <lyeguoming1995@gmail.com>
2024-07-30 08:08:53 +0900

    added activity schema
    Co-authored-by: kentaurse <kentaurse0212@gmail.com>
    Co-authored-by: glory1001 <lyeguoming1995@gmail.com>

--- src/activity/activity.module.ts
+++ src/activity/activity.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ActivityController } from './activity.controller';
 import { ActivityService } from './activity.service';
+import { ActivityShemaModule } from './schema/activity.schema';
 
 @Module({
+  imports: [ActivityShemaModule],
   controllers: [ActivityController],
-  providers: [ActivityService]
+  providers: [ActivityService],
 })
 export class ActivityModule {}
