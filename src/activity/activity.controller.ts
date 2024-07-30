b40388f9ce05aa6f8f33b09f47ba519db2fb4dd9
glory1001 <lyeguoming1995@gmail.com>
2024-07-30 16:22:40 +0900

    Activtiy crud updated
    Co-authored-by: kentaurse <kentaurse0212@gmail.com>
    Co-authored-by: glory1001 <lyeguoming1995@gmail.com>

--- src/activity/activity.controller.ts
+++ src/activity/activity.controller.ts
@@ -1,34 +1,32 @@
-import {Body, Controller, Get, Param, Post} from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
-import {ActivityService} from "./activity.service";
+import { ActivityService } from './activity.service';
 import { Activity } from './schema/activity.schema';
 import { CreateActivityDto } from './dto/create-activity.dto';
-import {Metrica} from "../metrics/schemas/metrica.schema";
-
+import { Metrica } from '../metrics/schemas/metrica.schema';
 @ApiTags('Activity')
-@Controller('activity')
+@Controller('v1/activity')
 export class ActivityController {
-    constructor(private readonly activityService: ActivityService) {}
+  constructor(private readonly activityService: ActivityService) {}
 
-    @ApiOperation({ summary: 'Create a new activity' })
-    @ApiResponse({ status: 200, type: Activity })
-    @Post()
-    createActivity(@Body() activityDto: CreateActivityDto) {
-        return this.activityService.createActivity(activityDto);
-    }
+  @ApiOperation({ summary: 'Create a new activity' })
+  @ApiResponse({ status: 200, type: Activity })
+  @Post()
+  createActivity(@Body() activityDto: CreateActivityDto) {
+    return this.activityService.createActivity(activityDto);
+  }
 
-    @ApiOperation({ summary: 'Get all activities' })
-    @ApiResponse({ status: 200, type: [Activity] })
-    @Get()
-    getAllActivities() {
-        return this.activityService.getAllActivities();
-    }
+  @ApiOperation({ summary: 'Get all activities' })
+  @ApiResponse({ status: 200, type: [Activity] })
+  @Get()
+  getAllActivities() {
+    return this.activityService.getAllActivities();
+  }
 
-    @ApiOperation({ summary: 'Get activity by id' })
-    @ApiResponse({ status: 200, type: Activity })
-    @Get('/:id')
-    getActivityById(@Param('id') id: string) {
-        return this.activityService.getActivityById(id);
-    }
-
-   }
+  @ApiOperation({ summary: 'Get activity by id' })
+  @ApiResponse({ status: 200, type: Activity })
+  @Get('/:id')
+  getActivityById(@Param('id') id: string) {
+    return this.activityService.getActivityById(id);
+  }
+}
--- src/activity/activity.service.ts
+++ src/activity/activity.service.ts
@@ -3,42 +3,31 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Activity, ActivityDocument } from './schema/activity.schema';
 import { Model } from 'mongoose';
 import { CreateActivityDto } from './dto/create-activity.dto';
-import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class ActivityService {
-    constructor(
-        @InjectModel(Activity.name) private activityModel: Model<ActivityDocument>,
-        private readonly usersService: UsersService,
-    ) {}
+  constructor(
+    @InjectModel(Activity.name) private activityModel: Model<ActivityDocument>,
+  ) {}
 
-    async createActivity(dto: CreateActivityDto) {
-        const createdActivity = await this.activityModel.create(dto);
-        const candidate = await this.usersService.getUserById(dto.userId);
+  async createActivity(dto: CreateActivityDto) {
+    const createdActivity = await this.activityModel.create(dto);
+    return createdActivity;
+  }
 
-        if (!candidate) {
-            throw new HttpException('No such user', HttpStatus.BAD_REQUEST);
-        }
+  async getAllActivities() {
+    return await this.activityModel.find().exec();
+  }
 
-        candidate.activity = createdActivity;
-        candidate.save();
-
-        return createdActivity;
+  async getActivityById(id: string) {
+    const isActivityExist = await this.activityModel.exists({ _id: id });
+    if (!isActivityExist) {
+      throw new HttpException(
+        { message: 'No such activity' },
+        HttpStatus.FORBIDDEN,
+      );
     }
 
-    async getAllActivities() {
-        return await this.activityModel.find().exec();
-    }
-
-    async getActivityById(id: string) {
-        const isActivityExist = await this.activityModel.exists({ _id: id });
-        if (!isActivityExist) {
-            throw new HttpException(
-                { message: 'No such activity' },
-                HttpStatus.FORBIDDEN,
-            );
-        }
-
-        return await this.activityModel.findOne({ _id: id }).exec();
-    }
+    return await this.activityModel.findOne({ _id: id }).exec();
+  }
 }
