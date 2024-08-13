7526aaa28da9ca289a1cf66741b4833693970307
kentaurse <kentaurse0212@gmail.com>
2024-08-13 10:00:36 +0900

    feat: complete

--- src/shared/config/database/mongodb.config.ts
+++ src/shared/config/database/mongodb.config.ts
@@ -5,10 +5,13 @@ export interface IMongoDBConfig {
   password: string;
   database: string;
   cluster: string;
+  key: string;
 }
 
 export const InitDatabaseConfig = (mongoDbConfig: IMongoDBConfig) => {
-  const url = `mongodb+srv://${mongoDbConfig.username}:${mongoDbConfig.password}@${mongoDbConfig.cluster}.szihj6t.mongodb.net/?retryWrites=true&w=majority`;
+  const url =
+    'mongodb+srv://victu-admin:123@victu-prod.i3lmgsp.mongodb.net/?retryWrites=true&w=majority';
+  // const url = `mongodb+srv://${mongoDbConfig.username}:${mongoDbConfig.password}@${mongoDbConfig.cluster}.${mongoDbConfig.key}.mongodb.net/?retryWrites=true&w=majority`;
 
   return MongooseModule.forRoot(url, { dbName: mongoDbConfig.database });
 };
